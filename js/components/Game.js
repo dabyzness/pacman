92169fbdd1a01c006ad36457793d5d88324279b9
Damian <bzdyrad@gmail.com>
2022-09-10 15:08:34 -0400

    add player to Game constructor

--- js/components/Game.js
+++ js/components/Game.js
@@ -1,9 +1,11 @@
 import { Tile } from "../components/Tile.js";
+import { Pacman } from "../components/Pacman.js";
 
 export class Game {
   constructor(board) {
     this.board = board.map((row) =>
       row.map((tileValue) => new Tile(tileValue))
     );
+    this.player = new Pacman();
   }
 }
