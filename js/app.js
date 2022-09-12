cd2d37c70e0b6fcd14db3139ae142be4436a095f
Damian <bzdyrad@gmail.com>
2022-09-12 09:54:11 -0400

    remove unused code from app.js

--- js/app.js
+++ js/app.js
@@ -6,15 +6,6 @@ let tiles;
 
 const possibleMovements = ["ArrowUp", "ArrowRight", "ArrowDown", "ArrowLeft"];
 
-let direction = "left";
-let inputs = [];
-
-let currPos = [
-  [24, 25, 26],
-  [31, 32, 33],
-  [38, 39, 40],
-];
-
 const pacmanLayoutRight = [
   ["top-left", "top-center", "top-right"],
   ["middle-left", "middle-center", "middle-right"],
@@ -39,16 +30,6 @@ const pacmanLayoutDown = [
   ["bottom-right dir-down", "middle-right dir-down", "top-right dir-down"],
 ];
 
-// const board = [
-//   [0, 0, 0, 0, 0, 0, 0],
-//   [0, 2, 1, 1, 1, 2, 0],
-//   [0, 1, 1, 1, 1, 1, 0],
-//   [0, 1, 1, 1, 1, 1, 0],
-//   [0, 1, 1, 1, 1, 1, 0],
-//   [0, 2, 2, 2, 2, 2, 0],
-//   [0, 0, 0, 0, 0, 0, 0],
-// ];
-
 const game = new Game(board);
 renderMap();
 
@@ -58,23 +39,8 @@ function renderMap() {
   game.board.forEach((row) => {
     row.forEach((tile) => {
       const tileEl = document.createElement("div");
-      // tile === 1
-      //   ? tileEl.setAttribute("class", "tile")
-      //   : tileEl.setAttribute("class", "tile edge");
-      // grid.appendChild(tileEl);
-      tileEl.setAttribute("class", tile.className);
 
-      // switch (tile.value) {
-      //   case 0:
-      //     tileEl.setAttribute("class", "tile edge");
-      //     break;
-      //   case 1:
-      //     tileEl.setAttribute("class", "tile");
-      //     break;
-      //   case 2:
-      //     tileEl.setAttribute("class", "tile small-pill");
-      //     break;
-      // }
+      tileEl.setAttribute("class", tile.className);
       grid.appendChild(tileEl);
     });
   });
@@ -131,111 +97,7 @@ window.addEventListener("keydown", ({ key }) => {
     console.log("hit");
     game.player.direction = key.replace("Arrow", "").toLowerCase();
   }
-
-  // switch (key) {
-  //   case "ArrowUp":
-  //     inputs.push("up");
-  //     break;
-  //   case "ArrowRight":
-  //     inputs.push("right");
-  //     break;
-  //   case "ArrowDown":
-  //     inputs.push("down");
-  //     break;
-  //   case "ArrowLeft":
-  //     inputs.push("left");
-  //     break;
-  // }
 });
-
-// function step() {
-// const dir = inputs.shift() || direction;
-// const nextPos = getNextPos(dir);
-
-// direction = dir;
-
-// if (isWall(nextPos, direction)) {
-//   renderPos();
-//   throw Error("You hit a wall");
-// }
-
-// unRenderPos();
-// currPos = nextPos;
-// renderPos();
-// }
-
-// function isWall(pos, dir) {
-//   let row = [];
-//   let col = [];
-
-//   switch (dir) {
-//     case "up":
-//       row = pos[0].map((tileNo) => Math.floor(tileNo / board[0].length));
-//       col = pos[0].map((tileNo) => tileNo % board[0].length);
-//       break;
-//     case "down":
-//       row = pos[2].map((tileNo) => Math.floor(tileNo / board[0].length));
-//       col = pos[2].map((tileNo) => tileNo % board[0].length);
-//       break;
-//     case "left":
-//       row.push(
-//         Math.floor(pos[0][0] / board[0].length),
-//         Math.floor(pos[1][0] / board[0].length),
-//         Math.floor(pos[2][0] / board[0].length)
-//       );
-//       col.push(
-//         pos[0][0] % board[0].length,
-//         pos[1][0] % board[0].length,
-//         pos[2][0] % board[0].length
-//       );
-//       break;
-//     case "right":
-//       row.push(
-//         Math.floor(pos[0][2] / board[0].length),
-//         Math.floor(pos[1][2] / board[0].length),
-//         Math.floor(pos[2][2] / board[0].length)
-//       );
-//       col.push(
-//         pos[0][2] % board[0].length,
-//         pos[1][2] % board[0].length,
-//         pos[2][2] % board[0].length
-//       );
-//       break;
-//   }
-
-//   for (let i = 0; i < row.length; i += 1) {
-//     if (!board[row[i]][col[i]]) {
-//       return true;
-//     }
-//   }
-
-//   return false;
-// }
-
-// function getNextPos(dir) {
-//   let nextPos = [];
-
-//   switch (dir) {
-//     case "up":
-//       nextPos = currPos.map((row) =>
-//         row.map((tileNo) => tileNo - board[0].length)
-//       );
-//       break;
-//     case "down":
-//       nextPos = currPos.map((row) =>
-//         row.map((tileNo) => tileNo + board[0].length)
-//       );
-//       break;
-//     case "left":
-//       nextPos = currPos.map((row) => row.map((tileNo) => tileNo - 1));
-//       break;
-//     case "right":
-//       nextPos = currPos.map((row) => row.map((tileNo) => tileNo + 1));
-//       break;
-//   }
-
-//   return nextPos;
-// }
 
 function unRenderPos(character) {
   character.currPos.forEach((row, i) => {
@@ -253,7 +115,5 @@ function unRenderPos(character) {
 setInterval(() => {
   unRenderPos(game.player);
   game.movePlayer();
-  // unRenderPos();
   renderPos();
-  // step();
 }, 100);
